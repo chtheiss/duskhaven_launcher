cc2e96268629be2bc1c5dda2b7b715a1792e58b3
Crypto2557 <theisz.cm@googlemail.com>
2023-04-05 00:06:40 +0200

    Fixed download speed on pause/resume

--- threads.py
+++ threads.py
@@ -38,12 +38,12 @@ class BackgroundTask(QThread):
             while self.paused:
                 self.msleep(100)
 
-        temp_dest_path = pathlib.Path(f"{self.dest_path}.part")
+        self.temp_dest_path = pathlib.Path(f"{self.dest_path}.part")
 
-        if not temp_dest_path.exists():
-            temp_dest_path.parent.mkdir(parents=True, exist_ok=True)
+        if not self.temp_dest_path.exists():
+            self.temp_dest_path.parent.mkdir(parents=True, exist_ok=True)
 
-        download_path = temp_dest_path or self.dest_path
+        download_path = self.temp_dest_path or self.dest_path
         total_size = self.total_size or download.fetch_size(self.url)
         headers = {}
         if download_path.exists():
@@ -55,11 +55,13 @@ class BackgroundTask(QThread):
 
         with requests.get(self.url, headers=headers, stream=True) as response:
             response.raise_for_status()
-            download_start_time = time.time()
-            start_file_size = (
-                temp_dest_path.stat().st_size if temp_dest_path.exists() else 0
+            self.download_start_time = time.time()
+            self.start_file_size = (
+                self.temp_dest_path.stat().st_size
+                if self.temp_dest_path.exists()
+                else 0
             )
-            current_file_size = start_file_size
+            self.current_file_size = self.start_file_size
 
             with open(download_path, mode) as file:
                 for chunk in response.iter_content(chunk_size=8192):
@@ -68,15 +70,15 @@ class BackgroundTask(QThread):
                             self.msleep(100)
                     if chunk:
                         file.write(chunk)
-                        file_stats = temp_dest_path.stat()
+                        file_stats = self.temp_dest_path.stat()
                         mod_time = file_stats.st_mtime
                         file_size = file_stats.st_size
-                        current_file_size = file_size
-                        time_diff = mod_time - download_start_time
+                        self.current_file_size = file_size
+                        time_diff = mod_time - self.download_start_time
                         progress_percent = file_size / total_size * 100
-                        current_file_size_in_mb = current_file_size / 1024 / 1024
+                        current_file_size_in_mb = self.current_file_size / 1024 / 1024
                         total_size_in_mb = total_size / 1024 / 1024
-                        downloaded_size = current_file_size - start_file_size
+                        downloaded_size = self.current_file_size - self.start_file_size
                         avg_download_speed = (
                             downloaded_size / (time_diff + 1e-5) / 1024 / 1024
                         )
@@ -96,8 +98,8 @@ class BackgroundTask(QThread):
                         )
                         self.progress_update.emit(progress_percent)
 
-        if temp_dest_path is not None:
-            shutil.move(temp_dest_path, self.dest_path)
+        if self.temp_dest_path is not None:
+            shutil.move(self.temp_dest_path, self.dest_path)
 
         etag = download.fetch_etag(self.url)
 
@@ -113,6 +115,14 @@ class BackgroundTask(QThread):
     def resume(self, dest_path):
         self.paused = False
         self.dest_path = dest_path
+        self.download_start_time = time.time()
+        if hasattr(self, "temp_dest_path"):
+            self.start_file_size = (
+                self.temp_dest_path.stat().st_size
+                if self.temp_dest_path.exists()
+                else 0
+            )
+            self.current_file_size = self.start_file_size
 
     def stop(self):
         self.quit()
